e153b8491e7e7e032383844c7760dc13b982308a
teddymillion <tedrosmilion19@gmail.com>
2024-05-29 20:06:29 +0300

    Update personal-data.js

--- utils/data/personal-data.js
+++ utils/data/personal-data.js
@@ -1,6 +1,6 @@
 export const personalData = {
   name: "Tewodros Million",
-  profile: '/Teddy.jpg',
+  profile: '/teddy.jpg',
   designation: "Software Developer",
   description: "My name is Tewodros Million. Currently I am a Software Engineering Student at Haramaya University . I am a quick learner with a self-learning attitude. I love to learn and explore new technologies and am passionate about problem-solving. I love almost all the stacks of web application development and love to make the web more open to the world. My core skill is based on JavaScript and I love to do most of the things using JavaScript. I am available for any kind of job opportunity that suits my skills and interests.",
   email: 'tedrosmilion19@gmail.com',
